cf8884df1e05e5bbce949055d0939335389dfaad
DEVDESIIGNN <muizharuna55@gmail.com>
2024-10-11 16:46:25 +0100

    feat: added floating whatsapp icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,11 @@ import { Testimonials } from "@/components/Testimonials";
 import { Pricing } from "@/components/Pricing";
 import { Process } from "@/components/Process";
 import { Topics } from "@/components/Topics";
+import { FloatingIcon } from "@/components/FloatingIcon";
 
 export default function Home() {
   return (
-    <>
+    <main className="relative">
       <div className="pt-14 pb-20 bg-container">
         <Header />
 
@@ -36,6 +37,8 @@ export default function Home() {
       <HaveAllah />
 
       <Footer />
-    </>
+
+      <FloatingIcon />
+    </main>
   );
 }
